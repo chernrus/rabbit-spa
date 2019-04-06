50aa5bd96f49fd7c4f73ccb24aae8632c0744f2b
Ruslan Chernov <chernruslan@gmail.com>
2019-04-06 20:45:52 +0300

    create webpack + react app

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname,'src','index.js'),
+  output: {
+    path: path.join(__dirname,'build'),
+    filename: 'index.bundle.js'
+  },
+  mode: process.env.NODE_ENV || 'development',
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules']
+  },
+  devServer: {
+    contentBase: path.join(__dirname,'src')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname,'src','index.html')
+    })
+  ]
+};
+
+// const HtmlWebPackPlugin = require("html-webpack-plugin");
+// const path = require('path');
+//
+// const htmlPlugin = new HtmlWebPackPlugin({
+//   template: "./src/index.html",
+//   filename: "./index.html"
+// });
+//
+// module.exports = {
+//   // module: {
+//   //   rules: [
+//   //     {
+//   //       test: /\.jsx?$/,
+//   //       exclude: /node_modules/,
+//   //       use: {
+//   //         loader: "babel-loader"
+//   //       }
+//   //     }
+//   //   ]
+//   // },
+//   plugins: [
+//     htmlPlugin
+//   ]
+// };
